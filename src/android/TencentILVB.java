563d741d184a1bb5abc8c95d166b8fe7379c89d3
alfred <57082212@qq.com>
2017-06-27 09:46:35 +0800

    初步实现接口集成

--- src/android/TencentILVB.java
+++ src/android/TencentILVB.java
@@ -1,6 +1,8 @@
 package cn.easecloud.cordova.tencent;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.webkit.WebView;
@@ -10,12 +12,17 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
+import org.json.JSONTokener;
+
+import com.tencent.ilivesdk.*;
+import com.tencent.ilivesdk.core.*;
 
 public class TencentILVB extends CordovaPlugin {
 
+    private Context context;
+    private Activity activity;
     private CordovaInterface cordova;
     private CordovaWebView webView;
-
 
     /**
      * Sets the context of the Command. This can then be used to do things like
@@ -29,6 +36,8 @@ public class TencentILVB extends CordovaPlugin {
         super.initialize(cordova, webView);
         this.webView = webView;
         this.cordova = cordova;
+        this.activity = cordova.getActivity();
+        this.context = this.activity.getApplicationContext();
     }
 
     /**
@@ -40,7 +49,7 @@ public class TencentILVB extends CordovaPlugin {
      * @return True if the action was valid, false if not.
      */
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
 
 //        this.cordova.requestPermission(this, 0, Manifest.permission.READ_CONTACTS);
 
@@ -56,10 +65,57 @@ public class TencentILVB extends CordovaPlugin {
 
             return true;
 
-        } else {
-            
-            return false;
+        } else if (action.equals("initSdk")) {
 
+            int appid = data.getInt(0);
+            int accountType = data.getInt(1);
+            ILiveSDK.getInstance().initSdk(this.context, appid, accountType);
+            return true;
+
+        } else if (action.equals("iLiveLogin")) {
+
+            String id = data.getString(0);
+            String sig = data.getString(1);
+            ILiveLoginManager.getInstance().iLiveLogin(id, sig, new ILiveCallBack() {
+                @Override
+                public void onSuccess(Object data) {
+                    JSONObject json = null;
+                    try {
+                        // object to JSONObject
+                        // https://stackoverflow.com/a/33056323/2544762
+                        json = (JSONObject) (new JSONTokener(data.toString()).nextValue());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        callbackContext.error("JSON parse object fail: " + data.toString());
+                    }
+                    callbackContext.success(json);
+                }
+
+                @Override
+                public void onError(String module, int errCode, String errMsg) {
+                    JSONObject obj = new JSONObject();
+                    try {
+                        obj.put("module", module);
+                        obj.put("errCode", errCode);
+                        obj.put("errMsg", errMsg);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        callbackContext.error("ERROR: " + errMsg);
+                    }
+                    callbackContext.error(obj);
+                }
+            });
+            return true;
+
+        } else if (action.equals("iLiveLogout")) {
+            return false;
+        } else if (action.equals("createRoom")) {
+            return false;
+        } else if (action.equals("joinRoom")) {
+            return false;
+        } else {
+            return false;
         }
     }
+
 }
--- www/ilvb.js
+++ www/ilvb.js
@@ -3,5 +3,57 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "TencentILVB", "greet", [name]);
-    }
+    },
+    initSdk: function (appId, accountType, successCallback, errorCallback) {
+        // 初始化 iLiveSDK
+        // int appId: 直播 appId
+        // int accountType: 直播 accountType
+        // Android: https://www.qcloud.com/document/product/268/7685#1-.E5.88.9D.E5.A7.8B.E5.8C.96
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "TencentILVB",
+            "initSdk",
+            [appId, accountType]
+        );
+    },
+    iLiveLogin: function (id, sig, successCallback, errorCallback) {
+        // 账号登录
+        // string id: 用户id,在直播过程中的唯一标识
+        // string sig: 鉴权的密钥Sig 如果是独立登录方式，是业务方后台计算生成后下发的
+        // Android: https://www.qcloud.com/document/product/268/7685#2-.E8.B4.A6.E5.8F.B7.E7.99.BB.E5.BD.95
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "TencentILVB",
+            "iLiveLogin",
+            [id, sig]
+        );
+    },
+    createRoom: function (id, option, successCallback, errorCallback) {
+        // 创建房间
+        // string id: 房间id,房间唯一标识，建议由业务方后台统一分配
+        // string option: 房间配置项 可以设置角色 权限 主播ID 摄像头参数等 具体参考类ILiveRoomOption
+        // Android: https://www.qcloud.com/document/product/268/7685#3-.E5.88.9B.E5.BB.BA.E6.88.BF.E9.97.B4
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "TencentILVB",
+            "createRoom",
+            [id, sig]
+        );
+    },
+    joinRoom: function (id, option, successCallback, errorCallback) {
+        // 加入房间
+        // string id: 房间id,房间唯一标识，建议由业务方后台统一分配
+        // string option: 房间配置项 可以设置角色 权限 主播ID 摄像头参数等 具体参考类ILiveRoomOption
+        // Android: https://www.qcloud.com/document/product/268/7685#4-.E5.8A.A0.E5.85.A5.E6.88.BF.E9.97.B4
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "TencentILVB",
+            "createRoom",
+            [id, sig]
+        );
+    },
 };
